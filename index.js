cb355342064eea85799f3f70f3b6d89b051eed69
Mallory Feuer <mallory.feuer@gmail.com>
2018-11-27 16:28:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function findMinAndRemoveSorted(array) {
   array.shift();
   return min;
 }
+
+function merge (a1, a2) {
+  sorted = []
+  while (a1.length !== 0 && a2.length !==0) {
+    if (a1[0] < a2[0]) {
+    
+    }
+  }
+}
