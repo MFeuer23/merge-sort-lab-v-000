bb9e5d9037d7e4830b4232f975b7427c533d1d06
Mallory Feuer <mallory.feuer@gmail.com>
2018-11-27 16:29:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ function merge (a1, a2) {
   sorted = []
   while (a1.length !== 0 && a2.length !==0) {
     if (a1[0] < a2[0]) {
-    
+      sorted.push(findMinAndRemoveSorted(a1))
+    } else {
+      sorted.push(findMinAndRemoveSorted(a1))
     }
   }
+  return sorted.concat(a1).concat(a2)
 }
